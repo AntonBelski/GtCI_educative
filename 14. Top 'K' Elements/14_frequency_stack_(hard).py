8defdabfabc6bd15062a4fed8d1a45e0aa09f1fb
AntonBelski <anthony.belski@gmail.com>
2022-04-08 14:20:38 +0300

    add solution for puzzle 14 for Top 'K' Elements pattern

---
+++ 14. Top 'K' Elements/14_frequency_stack_(hard).py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+
+
+class FrequencyStack:
+    def __init__(self):
+        self.freq = defaultdict(int)
+        self.group = defaultdict(list)
+        self.max_freq = 0
+
+    def push(self, val: int) -> None:
+        self.freq[val] += 1
+        self.group[self.freq[val]].append(val)
+        self.max_freq = max(self.max_freq, self.freq[val])
+
+    def pop(self) -> int:
+        val = self.group[self.max_freq].pop()
+        self.freq[val] -= 1
+
+        if not self.group[self.max_freq]:
+            self.max_freq -= 1
+
+        return val
+
+
+def main():
+    frequencyStack = FrequencyStack()
+    frequencyStack.push(1)
+    frequencyStack.push(2)
+    frequencyStack.push(3)
+    frequencyStack.push(2)
+    frequencyStack.push(1)
+    frequencyStack.push(2)
+    frequencyStack.push(5)
+    print(frequencyStack.pop())
+    print(frequencyStack.pop())
+    print(frequencyStack.pop())
+
+
+main()
