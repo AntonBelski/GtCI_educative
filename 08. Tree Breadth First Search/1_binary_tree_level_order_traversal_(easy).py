e579e7e25dbbb50f331ca507e791d86c4bb30b1d
AntonBelski <anthony.belski@gmail.com>
2022-05-27 01:49:28 +0300

    add solution for puzzle 1 for Tree Breadth First Search pattern

---
+++ 08. Tree Breadth First Search/1_binary_tree_level_order_traversal_(easy).py
@@ -0,0 +1,38 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+
+def traverse(root):
+    deq = deque()
+    result = []
+    if root:
+        deq.append([root, 0])
+    while deq:
+        node, level = deq.popleft()
+        if level == len(result):
+            result.append([])
+        result[level].append(node.val)
+        if node.left:
+            deq.append([node.left, level + 1])
+        if node.right:
+            deq.append([node.right, level + 1])
+
+    return result
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Level order traversal: " + str(traverse(root)))
+
+
+main()
