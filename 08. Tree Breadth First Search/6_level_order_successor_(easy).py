16d1712a6b1d34f810ee95f69de1b34ed17200c7
AntonBelski <anthony.belski@gmail.com>
2022-06-02 13:19:53 +0300

    add solution for puzzle 6 for Tree Breadth First Search pattern

---
+++ 08. Tree Breadth First Search/6_level_order_successor_(easy).py
@@ -0,0 +1,51 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+
+def find_successor(root, key):
+    deq = deque()
+    deq.append(root)
+
+    while deq:
+        node = deq.popleft()
+        if node.left:
+            deq.append(node.left)
+        if node.right:
+            deq.append(node.right)
+        if node.val == key and deq:
+            return deq[0]
+
+
+def main():
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+
+    result = find_successor(root, 3)
+    if result:
+        print(result.val)
+
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+
+    result = find_successor(root, 9)
+    if result:
+        print(result.val)
+
+    result = find_successor(root, 12)
+    if result:
+        print(result.val)
+
+
+main()
