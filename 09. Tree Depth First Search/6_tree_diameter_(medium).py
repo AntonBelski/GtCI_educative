ac0e317a5e0811e93fdf9af94b85378dbcf2aee6
AntonBelski <anthony.belski@gmail.com>
2022-06-16 18:08:47 +0300

    add solution for puzzle 6 for Tree Depth First Search pattern

---
+++ 09. Tree Depth First Search/6_tree_diameter_(medium).py
@@ -0,0 +1,43 @@
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class TreeDiameter:
+    def __init__(self):
+        self.max_diameter = 0
+
+    def find_diameter(self, root):
+        def dfs(node):
+            if not node:
+                return 0
+            left_path = dfs(node.left)
+            right_path = dfs(node.right)
+            self.max_diameter = max(self.max_diameter, left_path + right_path)
+            return max(left_path, right_path) + 1
+
+        dfs(root)
+        return self.max_diameter
+
+
+def main():
+    treeDiameter = TreeDiameter()
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.right.left = TreeNode(5)
+    root.right.right = TreeNode(6)
+    print("Tree Diameter: " + str(treeDiameter.find_diameter(root)))
+    root.left.left = None
+    root.right.left.left = TreeNode(7)
+    root.right.left.right = TreeNode(8)
+    root.right.right.left = TreeNode(9)
+    root.right.left.right.left = TreeNode(10)
+    root.right.right.left.left = TreeNode(11)
+    print("Tree Diameter: " + str(treeDiameter.find_diameter(root)))
+
+
+main()
